7e54d6543654f37fd53b4ff1a3a5654502eaa10f
Sithembiso Gama <sit_gam@rdi.local>
2023-12-05 22:46:53 +0200

    Completed 2 Java challenges.

---
+++ HackerRankChallenges.java
@@ -0,0 +1,73 @@
+
+import java.io.*;
+import java.util.Scanner;
+
+public class HackerRankChallenges 
+{
+	/*
+    Objective
+    In this challenge, we're going to use loops to help us do some simple math.
+
+    Task
+    Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: N x i = result.
+    Print  lines of output; each line  (where ) contains the  of  in the form:
+    N x i = result.
+
+    Sample Input: 2
+    Sample Output
+    2 x 1 = 2
+    2 x 2 = 4
+    2 x 3 = 6
+    2 x 4 = 8
+    2 x 5 = 10
+    2 x 6 = 12
+    2 x 7 = 14
+    2 x 8 = 16
+    2 x 9 = 18
+    2 x 10 = 20
+    */
+
+    private void printFewMultiples () 
+    {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        bufferedReader.close();
+        if(N >= 2 && N <= 20) {
+            for(int i = 1; i <= 10; i++) {
+                int res = N * i;
+                System.out.printf("%d x %d = %d\n", N, i, res);
+            }
+        }
+    }
+
+    /*
+    For each query, print the corresponding series on a new line. Each series must be printed in order as a single line of  space-separated integers.
+
+    Sample Input
+    2
+    0 2 10
+    5 3 5
+    Sample Output
+    2 6 14 30 62 126 254 510 1022 2046
+    8 14 26 50 98
+    */
+    private void PrepareJavaIntroductionJavaLoopsII () 
+    {
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int seriesValue = a; 
+            for(int k = 0; k < n; k++)
+            {
+                seriesValue += (Math.pow(2, k) * b);
+                System.out.print(seriesValue + " ");
+            }
+            System.out.println();
+            
+        }
+        in.close();
+    }
+}
