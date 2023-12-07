29c1fb2b7cb9e77b0aed9382b6a1d8a55446be93
Sithembiso Gama <sit_gam@rdi.local>
2023-12-07 23:56:33 +0200

    Added the Java Thread: Student Registration HackerRank Challenge.

--- HackerRankChallenges.java
+++ HackerRankChallenges.java
@@ -70,4 +70,24 @@ public class HackerRankChallenges
         }
         in.close();
     }
+	
+	/*
+     * Complete the 'fizzBuzz' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void fizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+            if((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            } else if((i % 3 == 0) && (!(i % 5 == 0))) {
+                System.out.println("Fizz");
+            } else if((!(i % 3 == 0)) && (i % 5 == 0)) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
 }
---
+++ RegistrationPortal.java
@@ -0,0 +1,64 @@
+//Java Threads: Students Registration - HackerRank Solution
+class RegistrationPortal {
+    private RegistrationPortal mInstance;
+    List<Student> mListRegisteredStudents;
+    void RegistrationPortal() {
+        //Constructor no args
+        mInstance = new RegistrationPortal();
+    }
+    
+    public RegistrationPortal getRegistrationPortal() {
+        //Returns the instance of the registration portal
+        return mInstance;
+    }
+    
+    void register(Student student) {
+        mListRegisteredStudents.add(student);
+    }
+    
+    List<Student> getRegisteredStudents() {
+        //Returns a list of registered students
+        return mListRegisteredStudents;
+    }
+}
+
+public class Student {
+	//Constructor
+	public Student(int studentId, int studentAge, String studentName, String studentDiscipline) {
+		super();
+		this.studentId = studentId;
+		this.studentAge = studentAge;
+		this.studentName = studentName;
+		this.studentDiscipline = studentDiscipline;
+	}
+	
+	int studentId;
+    int studentAge;
+	String studentName;
+	String studentDiscipline;
+	
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public int getStudentAge() {
+		return studentAge;
+	}
+	public void setStudentAge(int studentAge) {
+		this.studentAge = studentAge;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentDiscipline() {
+		return studentDiscipline;
+	}
+	public void setStudentDiscipline(String studentDiscipline) {
+		this.studentDiscipline = studentDiscipline;
+	}
+}
