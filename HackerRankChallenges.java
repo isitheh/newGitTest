5f3ae500a7d625e39ede86b2df726a2e638d3577
Sithembiso Gama <sit_gam@rdi.local>
2023-12-16 23:12:24 +0200

    Continue CoderByte Challenges.....

--- HackerRankChallenges.java
+++ HackerRankChallenges.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -42,6 +43,11 @@ public class HackerRankChallenges {
         
         //CoderByte Prep Challenges
         QuestionsMarksTester();  //Challenge 1
+        BracketMatcherTester();  //Challenge 2  
+        BracketCombinationsTester(); //Challenge 3
+        MinWindowSubstringTester(); //Challenge 4
+        CodelandUsernameValidationTester(); //Challenge 5
+        TreeConstructorTester(); //Challenge 6
     }
     
     private static void sockMerchantTester() {
@@ -413,10 +419,10 @@ public class HackerRankChallenges {
         List<Integer> mList = new ArrayList<>();
         for(int i = 0; i < arr.length; i++) {
             if(arr[i] == (i+1)) {
-                System.out.println("Value: " + arr[i] + " is in the right position.");
+                //System.out.println("Value: " + arr[i] + " is in the right position.");
                 continue;
             } else {
-                System.out.println("Value: " + arr[i] + " is in the wrong position.");
+                //System.out.println("Value: " + arr[i] + " is in the wrong position.");
             }
         }
         return mSwaps;
@@ -470,7 +476,7 @@ public class HackerRankChallenges {
     private static void QuestionsMarksTester() {
         //String str = "arrb6???4xxbl5???eee5";
         String str = "aa6?9";
-        QuestionsMarks(str);
+        System.out.println("CoderByte Challenge 1 - Questions Marks: " + QuestionsMarks(str));
     }
     /*
         Questions Marks
@@ -515,5 +521,292 @@ public class HackerRankChallenges {
         }
         System.out.println("return mResultStr = " + mResultStr);
         return mResultStr;
+    }
+
+    private static void BracketMatcherTester() {
+        String str = "the color re(d))()(()";
+        System.out.println("CoderByte Challenge 2 - Bracket Matcher: " + BracketMatcher(str));
+    }
+    
+    /*
+        Have the function BracketMatcher(str) take the str parameter being 
+        passed and return 1 if the brackets are correctly matched and each one 
+        is accounted for. Otherwise return 0. For example: if str is 
+        "(hello (world))", then the output should be 1, but if str is 
+        "((hello (world))" the the output should be 0 because the brackets do 
+        not correctly match up. Only "(" and ")" will be used as brackets. 
+        If str contains no brackets return 1.
+    */
+    private static String BracketMatcher(String str) {
+        // code goes here  
+        if(!str.contains("(") || !str.contains(")")) {
+            //If str contains no brackets return 1.
+            return "1";
+        }
+        
+        long countOpenBracket = str.chars().filter(mChar -> mChar == '(').count();
+        long countCloseBracket = str.chars().filter(mChar -> mChar == ')').count();
+        if(countOpenBracket == countCloseBracket) {
+            int openBrace = 0;
+            int closeBrace = 0;
+            for(int i = 0; i < str.length(); i++) {
+                if(str.charAt(i) == '(') openBrace++;
+                if(str.charAt(i) == ')') closeBrace++;
+                if(closeBrace > openBrace) {
+                    //If at any point we encounter a closing bracket before an opening bracket
+                    return "0";
+                }
+            }
+            return "1";
+        } else {
+            //If str contains more opening than closing brackets return 0.
+            //If str contains more closing than opening brackets return 0.
+            return "0";
+        }
+    }
+    
+    private static void BracketCombinationsTester() {
+        int num = 8;
+        System.out.println("CoderByte Challenge 3 - Bracket Combinations: " + BracketCombinations(num));
+    }
+    
+    /*
+        Bracket Combinations
+        Have the function BracketCombinations(num) read num which will be an 
+        integer greater than or equal to zero, and return the number of valid 
+        combinations that can be formed with num pairs of parentheses. 
+        For example, if the input is 3, then the possible combinations of 
+        3 pairs of parenthesis, namely: ()()(), are ()()(), ()(()), (())(), 
+        ((())), and (()()). There are 5 total combinations when the input is 3, 
+        so your program should return 5.
+    */
+    private static int BracketCombinations(int num) {
+        // code goes here  
+        //Given num number of pairs of brackets, how many combinations of brackets an be formed?
+        if(num == 0) return 1;
+        if(num == 1) return 1;
+        if(num == 2) return 2;
+        int result = (int) (factorial(num * 2)/(factorial(num + 1) * factorial(num)));
+        return result;
+    }
+
+    private static long factorial(int n) {
+        long factorialValue = 1;
+        for(int i = 1; i <= n; i++) {
+          factorialValue = factorialValue*i;
+        }
+        return factorialValue;
+    }
+
+    private static void MinWindowSubstringTester() {
+        System.out.println("CoderByte Challenge 4 - Min Window Substring: " + MinWindowSubstring(new String[] {"aaffhkksemckelloe", "fhea"}));
+        MinWindowSubstring(new String[] {"aaffhkksemckelloe", "fhea"});
+    }
+    
+    /*
+        Min Window Substring
+        Have the function MinWindowSubstring(strArr) take the array of strings 
+        stored in strArr, which will contain only two strings, the first 
+        parameter being the string N and the second parameter being a string K 
+        of some characters, and your goal is to determine the smallest substring
+        of N that contains all the characters in K. For example: if strArr is 
+        ["aaabaaddae", "aed"] then the smallest substring of N that contains the
+        characters a, e, and d is "dae" located at the end of the string. 
+        So for this example your program should return the string dae.
+
+        Another example: if strArr is ["aabdccdbcacd", "aad"] then the smallest 
+        substring of N that contains all of the characters in K is "aabd" which 
+        is located at the beginning of the string. Both parameters will be 
+        strings ranging in length from 1 to 50 characters and all of K's 
+        characters will exist somewhere in the string N. Both strings will 
+        only contains lowercase alphabetic characters.
+    */
+    
+    private static String MinWindowSubstring(String[] strArr) {
+        // code goes here 
+        //String array - strArr contains only two strings
+        String mainString = strArr[0];
+        String searchString = strArr[1];
+        HashMap<Integer, String> charIndex = new HashMap<>();
+        List<String> mNonRepeatChar = new ArrayList<>();
+        List<String> mRepeatChar = new ArrayList<>();
+        Integer baseIndex = 0;
+        List<Integer> subStringValidArray = new ArrayList<>();
+
+        for(int i = 0; i < mainString.length(); i++) {
+            for(int k = 0; k < searchString.length(); k++) {
+                if(mainString.charAt(i) == searchString.charAt(k)) {
+                    if(!charIndex.containsKey(i)) {
+                        if(!charIndex.containsValue(String.valueOf(mainString.charAt(i)))) {
+                            mNonRepeatChar.add(String.valueOf(mainString.charAt(i)));
+                        } else {
+                            mNonRepeatChar.remove(String.valueOf(mainString.charAt(i)));
+                            mRepeatChar.add(String.valueOf(mainString.charAt(i)));
+                        }
+                        charIndex.put(i, String.valueOf(mainString.charAt(i)));
+                    }
+                }
+            }  
+        }
+
+        for(Map.Entry<Integer, String> entry : charIndex.entrySet()) {
+            for(int j = 0; j < mNonRepeatChar.size(); j++) {
+                if((mNonRepeatChar.get(j)).equals(entry.getValue())) {
+                    baseIndex = entry.getKey();
+                    break;
+                }
+            }
+        }
+
+        subStringValidArray.add(baseIndex);
+        for (int x = 0; x < mRepeatChar.size(); x++) {
+            //In the main array, get the one closet to the base index.
+            List<Integer> repeatCharsIndex = new ArrayList<>();
+            for(Map.Entry<Integer, String> mEntry : charIndex.entrySet()) {
+              if(mEntry.getValue().equals(mRepeatChar.get(x))) {
+                repeatCharsIndex.add(mEntry.getKey());
+                mEntry.getValue();
+              }
+            }
+            //Get which value is closest to base index
+            int distance = Math.abs(repeatCharsIndex.get(0) - baseIndex);
+            int index = 0;
+            for (int k = 1; k < repeatCharsIndex.size(); k++) {
+                int kDistance = Math.abs(repeatCharsIndex.get(k) - baseIndex);
+                if(kDistance < distance) {
+                    index = k;
+                    distance = kDistance;
+                }
+            }
+            int validIndex = repeatCharsIndex.get(index);
+            if(!subStringValidArray.contains(validIndex)) {
+                subStringValidArray.add(validIndex);
+            }
+        }
+
+        int min = subStringValidArray.get(0);
+        for(int n = 0; n < subStringValidArray.size(); n++) {
+            if(min > subStringValidArray.get(n)) {
+              min = subStringValidArray.get(n);
+            }
+        }
+
+        int max = subStringValidArray.get(0);
+        for(int n = 0; n < subStringValidArray.size(); n++) {
+            if(max < subStringValidArray.get(n)) {
+              max = subStringValidArray.get(n);
+            }
+        }
+
+        String mMinSubString = mainString.substring(min, (max + 1));
+        return mMinSubString;
+    }
+    
+    private static void CodelandUsernameValidationTester() {
+        System.out.println("CoderByte Challenge 5 -  CodelandUsername Validation: " + 
+            CodelandUsernameValidation("u__hello_world123"));
+    }
+    
+    /*
+        Codeland Username Validation
+        Have the function CodelandUsernameValidation(str) take the str parameter
+        being passed and determine if the string is a valid username according 
+        to the following rules:
+
+        1. The username is between 4 and 25 characters.
+        2. It must start with a letter.
+        3. It can only contain letters, numbers, and the underscore character.
+        4. It cannot end with an underscore character.
+
+        If the username is valid then your program should return the string 
+        true, otherwise return the string false.
+    */     
+    private static String CodelandUsernameValidation(String str) {
+        // code goes here  
+        if(Character.isLetter(str.charAt(0))) {
+            //Username starts with a letter
+            if(str.length() > 3 && str.length() < 26) {
+                //The username is between 4 and 25 characters.
+                if(str.charAt(str.length() - 1) != '_') {
+                    //It cannot end with an underscore character..
+                    int mStringLen = str.length();
+                    int counter = 0;
+                    for(int i = 0; i < mStringLen; i++) {
+                        if(Character.isLetter(str.charAt(i))) counter++;
+                        if(Character.isDigit(str.charAt(i))) counter++;
+                        if(str.charAt(i) == '_') counter++;
+                    }
+                    if(counter == mStringLen) {
+                        //The string contains only letters, numbers and underscore
+                        //All valid conditions have been met.
+                        return "true";
+                    } else {
+                        //Not all valid conditions have been met.
+                        return "false";
+                    }
+                }  else {
+                    //The username ends with an underscore character.
+                    return "false";
+                }
+            } else {
+                //The username is not between 4 and 25 characters.
+                return "false";
+            }
+        } else {
+          //Username does not start with a letter
+          return "false";
+        }
+    }
+
+    private static void TreeConstructorTester() {
+        System.out.println("CoderByte Challenge 6 -  Tree Constructor: " + 
+                TreeConstructor(new String[] {"(2,5)", "(2,6)"}));
+    }
+    /*
+        Have the function TreeConstructor(strArr) take the array of strings 
+        stored in strArr, which will contain pairs of integers in the following 
+        format: (i1,i2), where i1 represents a child node in a tree and the 
+        second integer i2 signifies that it is the parent of i1.Your program 
+        should, in this case, return the string true because a valid binary tree
+        can be formed. If a proper binary tree cannot be formed with the integer
+        pairs, then return the string false. All of the integers within the tree
+        will be unique, which means there can only be one node in the tree with 
+        the given integer value.
+    */
+    private static String TreeConstructor(String[] strArr) {
+        // code goes here  
+        HashMap<Integer, List<String>> mCombos = new HashMap<>();
+        List<String> allChildren = new ArrayList<>();
+        for(int i = 0; i < strArr.length; i++) {
+            List<String> mKeysArray = new ArrayList<>();
+            List<String> mValuesArray = new ArrayList<>();
+            String mString = strArr[i];
+            mString = mString.replace("(", "").replace(")", "");
+            String[] mArr = mString.split(",");
+            if(!mKeysArray.contains(mArr[1])) {
+                mKeysArray.add(mArr[1]);
+                for(int x = 0; x < strArr.length; x++) {
+                    String xString = strArr[x];
+                    xString = xString.replace("(", "").replace(")", "");
+                    String[] xArr = xString.split(",");
+                if(mArr[1].equals(xArr[1])) {
+                    if(!mValuesArray.contains(xArr[0])) {
+                        mValuesArray.add(xArr[0]);
+                    } else {
+                        return "false";
+                    }
+                }
+            }
+            if(!allChildren.contains(mArr[0])) {
+              allChildren.add(mArr[0]);
+            } else {
+              return "false";
+            }
+          }
+          if(mValuesArray.size() > 2) {
+            return "false";
+          } 
+        }
+        return "true";
     }
 }
