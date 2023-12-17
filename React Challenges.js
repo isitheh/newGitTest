e427a4420ffe4a357f81883966f0ef5dfed55b07
Sithembiso Gama <sit_gam@rdi.local>
2023-12-17 16:07:31 +0200

    Continue CoderByte Challenges. Also added a js file for react challenges.

---
+++ React Challenges.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+/*
+	React Button Toggle
+	We provided some simple React template code. Your goal is to modify the component so 
+	that you can properly toggle the button to switch between an ON state and an OFF state. 
+	When the button is on and it is clicked, it turns off and the text within it changes 
+	from ON to OFF and vice versa. Make use of component state for this challenge.
+
+	You are free to add classes and styles, but make sure you leave the component ID's 
+	and clases provided as they are. Submit your code once it is complete 
+	and our system will validate your output.
+*/
+const Toggle = () => {
+  const [stateToggled, setStateToggled] = useState(false);
+  const toggleHandler = () => {
+    setStateToggled(!stateToggled);
+  }
+
+  return(
+    <>
+      <button onClick={toggleHandler}>
+        {stateToggled ? 'ON' : 'OFF'}
+      </button>
+    </>
+  );
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<Toggle />);
